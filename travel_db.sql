7cdd770bd37421215072f08b7f49af6fecbf1c21
tahirpukhta <pukhta.tahir@gmail.com>
2025-02-08 10:39:29 +0530

    updated the name of the column 'password' to 'password_hash'

--- travel_db.sql
+++ travel_db.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password_hash VARCHAR(255) NOT NULL --changed from 'password'.
 );
 
 CREATE TABLE hotels (
